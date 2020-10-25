2aca4df10be0887a5a36a57f63754e92af8ab6e7
mykola <kolasvitlovskij@gmail.com>
2020-10-25 21:48:00 +0200

    add two functions in "ConsoleApplication1.cpp"

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -3,6 +3,26 @@
 #include <math.h>
 using namespace std;
 
+int sum()
+{
+	int y = 0;
+	for (int i = 0; i <= 10; i++)
+	{
+		y += i;
+	}
+	y = log10(y);
+	return y;
+}
+
+int dob(int b)
+{
+	for (int n = 1; n <= 10; n++)
+	{
+		b *= n;
+	}
+	return: b;
+}
+
 int main()
 {
 	SetConsoleCP(1251);
@@ -11,22 +31,12 @@ int main()
 	/*double x, y;
 	cout << "Введіть x: ";
 	cin >> x;
-	y = 0;
-	for (int i = 0; i <= 10; i++)
-	{
-		y += i;
-	}
-	y = log10(y);
-	cout << "Результат обчислення: " << y + pow(x, 5);*/
+	cout << "Результат обчислення: " << sum() + pow(x, 5);*/
 	//Програма №2 визначення значення добутку
 	double z, y, b = 1;
 	cout << "Введіть y: ";
 	cin >> y;
-	for (int n = 1; n <= 10; n++)
-	{
-		b *= n;
-	}
-	z = (pow(y, 3) + b + 2) / pow(y, 2) + 5;
+	z = (pow(y, 3) + dop(b) + 2) / pow(y, 2) + 5;
 	cout << "Результат обчислення: " << z;
 }
 
