9b0afaec082fec497951214812c1468ecd69ef72
mykola <kolasvitlovskij@gmail.com>
2020-11-16 17:11:49 +0200

    add calculate factorial

--- new__file.cpp
+++ new__file.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include "windows.h"
 
 using namespace std;
 
 int main()
 {
-    cout<<"Hello World";
-
+	setlocale(LC_ALL, ".1251");
+    cout<<"Введіть факторіал числа: ";
+    int n, f = 1;
+    cin >> n;
+    for(int i = 1; i < n; i++)
+    {
+    	f *= i;
+    }
+    cout << f;
     return 0;
 }
